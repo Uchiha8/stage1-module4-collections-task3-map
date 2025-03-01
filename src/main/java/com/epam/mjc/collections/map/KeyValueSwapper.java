96a56ce401fb49849cff03d762a0a2ff01cd8244
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-01 14:10:27 +0500

    Implement word repetition map and key-value swap functionality

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            String value = entry.getValue();
+            int key = entry.getKey();
+            swappedMap.put(value, Math.min(swappedMap.getOrDefault(value, key), key));
+        }
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,15 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> maps = new HashMap<>();
+        for (String str : List.of(sentence.split(" "))) {
+            maps.put(str.toLowerCase(), maps.containsKey(str.toLowerCase()) ? maps.get(str.toLowerCase()) + 1 : 1);
+        }
+        return maps;
     }
 }
