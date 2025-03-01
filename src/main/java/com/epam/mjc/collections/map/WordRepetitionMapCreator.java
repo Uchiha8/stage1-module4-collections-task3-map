93c9013182e29113b55ad7a22c22f080f3613111
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-01 14:11:56 +0500

    Add null and empty string check in word repetition map creation

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,14 +1,16 @@
 package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
         Map<String, Integer> maps = new HashMap<>();
-        for (String str : List.of(sentence.split(" "))) {
-            maps.put(str.toLowerCase(), maps.containsKey(str.toLowerCase()) ? maps.get(str.toLowerCase()) + 1 : 1);
+        if (sentence == null || sentence.isEmpty()) {
+            return maps;
+        }
+        for (String str : sentence.toLowerCase().split("\\s+")) {
+            maps.put(str, maps.getOrDefault(str, 0) + 1);
         }
         return maps;
     }
